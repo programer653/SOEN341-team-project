1a2ddde5fb867fa8c3df1195323f2926b0e08523
Asha Islam <ashaislam31@gmail.com>
2022-02-17 03:35:21 -0500

    Adding the promo page into the website.

--- php/promotionPage.php
+++ php/promotionPage.php
@@ -1,1 +1,75 @@
-<?php
+<?php include '../php/header.php'; ?>
+<!--Products-->
+<div class="products">
+    <div class="products-header">
+        <h2 class="title">Promotions</h2>
+<!--        <a href="../php/Homepage.php">-->
+<!--            <h4>Go Back <i>&#x2192</i></h4>-->
+<!--        </a>-->
+    </div>
+    <div class="row">
+        <div class="products-row1">
+            <div class="products-col">
+                <a href="cut-chuck-roast.html"> <!-- changer le file name avec fichier de ami-->
+                    <img src="../images/Meat/cut-chuck-roast.jpg" alt="#">
+                    <h4>Cut Chuck Roast</h4>
+                </a>
+                <p class="new-price">
+                    <strike>$22.02</strike>
+                    <strong>$20.00 avg. ea.</strong>
+                </p>
+                <p>(300g avg.)</p>
+                <p><strike>$4.39</strike>$4.00/kg</p>
+                <p><strike>$1.99</strike>$1.75/lb.</p>
+            </div>
+            <div class="products-col">
+                <a href="chickenLegs.html"> <!-- changer le file name avec fichier de ami-->
+                    <img src="../images/Meat/chickenLegs.jpg" alt="#">
+                    <h4>Chicken Legs</h4>
+                </a>
+                <p class="new-price">
+                    <strike>$3.85</strike>
+                    <strong>$3.00 avg. ea.</strong>
+                </p>
+                <p>(500g avg.)</p>
+                <p><strike>$7.69</strike>$6.00/kg</p>
+                <p><strike>$3.49</strike>$2.50/lb.</p>
+            </div>
+        </div>
+        <div class="products-row2">
+            <div class="products-col">
+                <a href="porkTenderloin.html"> <!-- changer le file name avec fichier de ami-->
+                    <img src="../images/Meat/porkTenderloin.jpeg" alt="#">
+                    <h4>Pork Tenderloin</h4>
+                </a>
+                <p class="new-price">
+                    <strike>$8.42</strike>
+                    <strong>$8.00 avg. ea.</strong>
+                </p>
+                <p>(450g avg.)</p>
+                <p><strike>$7.69</strike>$6.00/kg</p>
+                <p><strike>$18.72</strike>$15.00/lb.</p>
+            </div>
+            <div class="products-col">
+                <a href="LeanGroundBeef.html"> <!-- changer le file name avec fichier de ami-->
+                    <img src="../images/Meat/LeanGroundBeef.jpg" alt="#">
+                    <h4>Lean Ground Beef</h4>
+                </a>
+                <p class="new-price">
+                    <strike>$3.00</strike>
+                    <strong>$2.00 avg. ea.</strong>
+                </p>
+                <p>(100g avg.)</p>
+                <p><strike>$13.00</strike>$10.00/kg</p>
+                <p><strike>$1.34</strike>$1.20/lb.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--footer-->
+<?php include '../php/footer.php'; ?>
+</body>
+
+</html>
